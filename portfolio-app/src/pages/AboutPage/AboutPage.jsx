afb0a84ca40aaf3e85222bc542943673cec957ce
shivam1611 <iamshivamsharma755@gmail.com>
2024-04-08 19:11:13 +0530

    desktop Version  completed

--- portfolio-app/src/pages/AboutPage/AboutPage.jsx
+++ portfolio-app/src/pages/AboutPage/AboutPage.jsx
@@ -3,95 +3,96 @@ import Circle from "../../Components/Circle/Circle";
 import MainContainer from "../../Components/MainContainer/MainContainer";
 import Navbar from "../../Components/Navbar/Navbar";
 import style from "./AboutPage.module.css";
-import ContainerTitle from '../../Components/ComponentTitle/ComponentTitle'
+import ContainerTitle from "../../Components/ComponentTitle/ComponentTitle";
 import { motion } from "framer-motion";
 
 function AboutPage() {
   return (
     <div>
       <Navbar />
-      <Circle top={"-19rem"} left={"-15rem"}/>
+      <Circle bottom={"19rem"} right={"-25rem"} />
       <MainContainer>
-      <div className={style.about_component}>
-         <ContainerTitle title={"About me"}/>
+        <div className={style.about_component}>
+          <ContainerTitle title={"About me"} />
           <div className={style.timeline_container}>
             <h1 className={style.timeline_title}>My Education</h1>
             <div className={style.timeline_box}>
-              <ActionBox
-                isleft={true}
-                timeline={"2017 - 2018"}
-                timeline_title={"High School "}
-                details={
-                  "Completed my High School Degree from State Board with 78% from SVM School, Kotdwara, 246149."
-                }
-              />
-              <ActionBox
-                isleft={false}
-                timeline={"2019 - 2020"}
-                timeline_title={"Senior Secondary (10+2)"}
-                details={
-                  "Completed my Degree from State Board with 78% from SVM School, Kotdwara, 246149."
-                }
-              />
-              <ActionBox
-                isleft={true}
-                timeline={"2020 - 2022"}
-                timeline_title={"Diploma in Computer Science "}
-                details={"Pursued the diploma in cse branch"}
-                img={
-                  "https://www.gpkotdwar.org.in/images/gallery/l8THHpO6.jpg"
-                }
-              />
-              <ActionBox
-                isleft={false}
-                timeline={"2022 - 2025"}
-                timeline_title={"University Of Mysore"}
-                details={
-                  "my name is shivam sharma and i a he is the biggest chutiya college in the world"
-                }
-                img={
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq25wBcbXkIUAg1yyYvwNMT--QIk76V1QFkI0rIdt5Kw&s"
-                }
-              />
+              <div className={`${style.action_container} ${style.left_container}`}>
+                <ActionBox
+                  isleft={true}
+                  timeline={"2017 - 2018"}
+                  timeline_title={"High School "}
+                  location={"SVM Inter College, Kotwdara"}
+                  details={
+                    "Completed my High School Degree from State Board with 78%."
+                  }
+                />
+              </div>
+              <div className={`${style.action_container} ${style.right_container}`}>
+                <ActionBox
+                  isleft={false}
+                  timeline={"2019 - 2020"}
+                  timeline_title={"Senior Secondary (10+2)"}
+                  location={"SVM Inter College, Kotdwara"}
+                  details={
+                    "Completed my Degree from State Board with 78% from SVM School, Kotdwara, 246149."
+                  }
+                />
+              </div>
+              <div className= {`${style.action_container} ${style.left_container}`}>
+                <ActionBox
+                  isleft={true}
+                  timeline={"2020 - 2022"}
+                  timeline_title={"Diploma in Computer Science "}
+                  location={"Govt. Polytechnic Kotdwara"}
+                  details={"Pursued the diploma in Computer Science & Engineering with 9 cgpa from Uttrakhand Board Of Technical Education"}
+                  img={
+                    "https://www.gpkotdwar.org.in/images/gallery/l8THHpO6.jpg"
+                  }
+                />
+              </div>
+              <div className= {`${style.action_container} ${style.right_container}`}>
+                <ActionBox
+                  isleft={false}
+                  timeline={"2022 - 2025"}
+                  timeline_title={"Bachelors in Computer Applications"}
+                  location={"University Of Mysore"}
+                  details={
+                    "Currently completing my Bachelors Degree in Computer Applications (Online) from Mysore University"
+                  }
+                  img={
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq25wBcbXkIUAg1yyYvwNMT--QIk76V1QFkI0rIdt5Kw&s"
+                  }
+                />
+              </div>
             </div>
           </div>
           <div className={style.timeline_container}>
             <h1 className={style.timeline_title}>My Experience</h1>
             <div className={style.timeline_box}>
-              <ActionBox
-                isleft={true}
-                timeline={"2022 - 2025"}
-                timeline_title={"University Of Mysore"}
-                details={
-                  "my name is shivam sharma and i a he is the biggest chutiya college in the world"
-                }
-                img={
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq25wBcbXkIUAg1yyYvwNMT--QIk76V1QFkI0rIdt5Kw&s"
-                }
-              />
-              <ActionBox
-                isleft={false}
-                timeline={"2020 - 2022"}
-                timeline_title={"Diploma in Computer Science "}
-                details={"Pursued the diploma in cse branch"}
-              />
-
-              <ActionBox
-                isleft={true}
-                timeline={"2019 - 2020"}
-                timeline_title={"Intermediate School (10+2)"}
-                details={
-                  "Completed my schooling (10+2) from State Board in PCM branch with 80 percentage"
-                }
-              />
-              <ActionBox
-                isleft={false}
-                timeline={"2017 - 2018"}
-                timeline_title={"High School "}
-                details={
-                  "Completed my schooling (10+2) from State Board in PCM branch with 80 percentage"
-                }
-              />
+              <div className={`${style.action_container} ${style.left_container}`}>
+                <ActionBox
+                  isleft={true}
+                  timeline={"2022 - 2023"}
+                  timeline_title={"TGT Teacher"}
+                  location={"Bal Bharti Public School, Kotdwara"}
+                  details={
+                    "I have worked as a junior teacher in school and also worked in management. "
+                  }
+                />
+              </div>
+              <div className={`${style.action_container} ${style.right_container}`}>
+                <ActionBox
+                  isleft={false}
+                  timeline={"2023 - 2024"}
+                  timeline_title={"Apprentice Engineer "}
+                  location={"Bharat Electronics Limited, Ktd."}
+                  details={
+                    "Worked as an Apprentice Engineer for 1 year under BEL Kotdwara, Ministry of External Affairs"
+                  }
+                />
+              </div>
+             
             </div>
           </div>
         </div>
