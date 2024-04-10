c16ec11954ed7908116f2583284ebbbf8764abd8
shivam1611 <iamshivamsharma755@gmail.com>
2024-04-10 20:58:05 +0530

    resume updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -65,7 +65,7 @@ function HomePage() {
             <a
               className="link_under_button"
               target="_blank"
-              href="https://drive.google.com/file/d/1rB1dOgspKOwmQhJ3xyRbpjartIgEhQYT/view?usp=sharing"
+              href="https://drive.google.com/file/d/1OWcujKOMFA28iK6YHPyRPUSWYYY-Q9IV/view?usp=drive_link"
             >
             
               Download CV
