7b0caef0797cfb02b259b9785a8b6a2285b8fb4d
shivam1611 <iamshivamsharma755@gmail.com>
2024-08-21 21:22:58 +0530

    Ussual Changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
 function App() {
   return (
     <>
+    
       <BrowserRouter>
       <ScrollToTop/>
         <Routes>
@@ -22,6 +23,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
+      
     </>
   );
 }
--- src/Components/ProjectCard/projectCard.jsx
+++ src/Components/ProjectCard/projectCard.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+
 import styles from "./projectCard.module.css";
+
 
 
 function projectCard({
